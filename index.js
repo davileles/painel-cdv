fcbd516484e9f7193d2af4440cfd43847a1846c7
davileles <65415498+davileles@users.noreply.github.com>
2026-08-10 08:43:32 -0300

    fix(cors): permitir X-TSP-Token e X-CDV-Env no preflight (corrige Failed to fetch no distribuidor TSP)

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.use(express.json({ limit: '20mb' }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-TSP-Token, X-CDV-Env');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
